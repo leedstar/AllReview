d8b898370495262c60c91a6c6e1672264e93c480
DaeGyuLEE <wowdkck@naver.com>
2021-08-18 01:50:46 +0900

    Create Customer table & node.js Database Connection settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs'); 
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,34 +7,28 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const conn = mysql.createConnection({
+     host : conf.host,
+     user : conf.user,
+     password : conf.password,
+     port : conf.port,
+     database : conf.database
+});
+
+conn.connect();
+
+
 app.get('/api/customers', (rep, res) => {
-    res.send(
-        [
-            {
-                'id': 1,
-                'image': 'https://placeimg.com/64/64/1',
-                'name': '홍길동zzz',
-                'birthday': '961222',
-                'gender': '남자',
-                'job': '대학생'
-            },
-            {
-                'id': 2,
-                'image': 'https://placeimg.com/64/64/2',
-                'name': '홍길동',
-                'birthday': '960305',
-                'gender': '남자',
-                'job': '프로그래머'
-            },
-            {
-                'id': 3,
-                'image': 'https://placeimg.com/64/64/3',
-                'name': '삼삼삼',
-                'birthday': '555555',
-                'gender': '여자',
-                'job': '디자이너'
-            }
-        ]
+    conn.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, field) => {
+            res.send(rows);
+
+        }
     )
 })
 
